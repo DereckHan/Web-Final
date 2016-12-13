37968fc2901000cd7551eeab253a97ecfb354720
DereckHan <pitthhy@gmail.com>
2016-12-12 21:33:32 -0500

    add Json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,7 +30,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-//    port(Integer.valueOf(System.getenv("PORT")));
+    port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
       Gson gson = new Gson();
 
